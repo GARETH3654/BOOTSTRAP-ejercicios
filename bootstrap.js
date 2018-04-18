5890a802a19ad510961b48b9e0ffc5135e542ef2
ivan lezama nuñez <gareth.ivc@gmail.comwqqwwqgit config --global user.email gareth.ivc@gmail.com>
2018-04-17 21:50:49 -0500

    second repositry bootstrap

--- bootstrap.js
+++ bootstrap.js
@@ -1,3 +1,32 @@
-console.log('Hola');
-$('body').css('background-color','LightBlue')
-$('h1').css('color','red');
+console.log('Hola')
+$('h1').css('color','white');
+$('navbar-brand')
+
+$('.nav-link').on('click',function(event){
+  var target=event.currentTarget.getAttribute('data-ivan');
+  $('html,body').animate({
+    scrollTop: $(target).offSet().top
+  },1000)
+})
+
+//butto interaction
+$('.modal-button').on('click',function(event) {
+  $('.custom-modal').toggle('not-showing');
+  if($ (event.currentTarget).hasClass('skill-button')==true) {
+    $('.skill1-modal').show();
+    $('.skill2-modal').hide();
+    $('.skill3-modal').hide();
+  }else if ($ (event.currentTarget).hasClass('skill2-button')==true) {
+    $('.skill1-modal').hide();
+    $('.skill2-modal').show();
+    $('.skill3-modal').hide();
+  } else {
+    $('.skill1-modal').hide();
+    $('.skill2-modal').hide();
+    $('.skill3-modal').show();
+
+  }
+});
+$('.modal-background, .close-modal i' ).on('click',function(){
+  $('.custom-modal').toggle('not-showing')
+})
